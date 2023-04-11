e8460bb632427e51e34f48570c6f17bccd7aab3b
randomdude <DoiGaMay@gmail.com>
2023-04-12 00:55:20 +0700

    aaa

---
+++ GameModules/Universal.lua
@@ -0,0 +1,9 @@
+
+
+local MainGui = shared.MainGui
+
+local Combattab = MainGui:newtab("Combat")
+local BlantantTab = MainGui:newtab("Blantant")
+local UtilityTab = MainGui:newtab("Utility")
+local CosmeticTab = MainGui:newtab("Cosmetic")
+
