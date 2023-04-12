67c77f64c5f6040b98fa9aca1375c48b9ce3c089
randomdude <DoiGaMay@gmail.com>
2023-04-12 09:35:08 +0700

    exper

--- GameModules/Universal.lua
+++ GameModules/Universal.lua
@@ -7,3 +7,6 @@ local BlantantTab = MainGui:newtab("Blantant")
 local UtilityTab = MainGui:newtab("Utility")
 local CosmeticTab = MainGui:newtab("Cosmetic")
 
+
+local GetCombatTab = MainGui:FindTab("Combat")
+GetCombatTab:RemoveTab()
