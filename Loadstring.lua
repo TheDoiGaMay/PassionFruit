7a446397f6f94adc1048b092d2743fc1585dfd48
randomdude <DoiGaMay@gmail.com>
2023-06-21 18:06:32 +0700

    a

--- Loadstring.lua
+++ Loadstring.lua
@@ -1,4 +1,1 @@
-
-loadstring(game:HttpGet("https://raw.githubusercontent.com/randomdude11135/PassionFruit/master/MainScript.lua", true))
-
-
+loadstring(game:HttpGet("https://raw.githubusercontent.com/randomdude11135/PassionFruit/master/MainScript.lua", true))()
