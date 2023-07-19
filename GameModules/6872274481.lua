47612c1636d03f1f9ac702d2e398c5bc5c441ca9
randomdude <DoiGaMay@gmail.com>
2023-07-20 02:48:10 +0700

    a

--- GameModules/6872274481.lua
+++ GameModules/6872274481.lua
@@ -1192,10 +1192,16 @@ do
         local GetChestDelayProperty = shared.IClientToggledProperty["Chest"]["Grab Delay"]
 
         if BedwarLibrary.AppController:isAppOpen("ChestApp") then
-            if shared.IClientToggledProperty["Chest"]["Skywars Only?"] == true and BedwarLibrary.ClientStoreHandler:getState().Game.queueType:find("skywars") then else return end
+            print(chest.Value.Name)
+            if shared.IClientToggledProperty["Chest"]["Skywars Only?"] == true then
+                if BedwarLibrary.ClientStoreHandler:getState().Game.queueType:find("skywars") then
+                    
+                else
+                    return
+                end
+            end
             local chest = LocalPlayer.Character:FindFirstChild("ObservedChestFolder")
             local chestitems = chest and chest.Value and chest.Value:GetChildren() or {}
-            print(chest.Value.Name)
             if #chestitems > 0 then
                 for i3,v3 in pairs(chestitems) do
                     if v3:IsA("Accessory") then
