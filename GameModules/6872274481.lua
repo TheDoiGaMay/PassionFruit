a5cee780ed5696109501cb83e99948ba8e1d7a1d
randomdude <DoiGaMay@gmail.com>
2023-07-19 20:17:42 +0700

    a

--- GameModules/6872274481.lua
+++ GameModules/6872274481.lua
@@ -1148,13 +1148,11 @@ do
 
             task.delay(0.075, function()
                 local block, pos2 = getPlacedBlock(pos)
-                if block or pos2 then
-                    switchToAndUseTool("gumdrop_bounce_pad",true)
-                    BreakBlock(pos)
-                    task.wait(0.1)
-                    switchItem(GetCurrentEquuipped.Object,true)
-                end
-              
+               
+                switchToAndUseTool("gumdrop_bounce_pad",true)
+                BreakBlock(pos)
+                task.wait(0.1)
+                switchItem(GetCurrentEquuipped.Object,true)              
                 --BedwarLibrary.BlockEngineClientEvents.DamageBlock:fire(block.Name, pos, block) 
             end)
 
